c5a7e8963eaef9f3d4508b62812a214f17e4113a
Sennar2 <danieleraicaldo@gmail.com>
2025-10-29 02:39:47 +0000

    Update FinancialHeader.jsx

--- src/components/financial/FinancialHeader.jsx
+++ src/components/financial/FinancialHeader.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
-import "../app/globals.css";
+import "./globals.css";
 
 export default function FinancialHeader() {
   const router = useRouter();
